a0c4963a471836afbe85ec4e55ff5ec1316c7c81
prostosite570 <61553765+prostosite570@users.noreply.github.com>
2020-02-28 17:19:48 +0200

    1.0

---
+++ script.js
@@ -0,0 +1,56 @@
+ $("doucument").ready(function(){
+	$(".box6-simple-btn").click(function(){
+		let x = this.id
+		if (x == "box6-simple-btn1"){
+			document.getElementById('box6-simple-btn1').style.background = "#ddd"; 
+			document.getElementById('box6-simple-btn2').style.background = "#bbb"; 
+			document.getElementById('box6-simple-btn3').style.background = "#bbb"; 
+			
+			document.getElementById('box6-simple-btn1').style.borderBottom ="none";
+			document.getElementById('box6-simple-btn2').style.borderBottom ="solid #000 1px";
+			document.getElementById('box6-simple-btn3').style.borderBottom ="solid #000 1px";
+
+			document.getElementById('box6-more1').style.display = "block";
+			document.getElementById('box6-more2').style.display = "none";
+			document.getElementById('box6-more3').style.display = "none";
+
+			document.getElementById('box6-simple-main1').style.display = "block";
+			document.getElementById('box6-simple-main2').style.display = "none";
+			document.getElementById('box6-simple-main3').style.display = "none";
+		}	
+		else if(x == "box6-simple-btn2"){
+			document.getElementById('box6-simple-btn1').style.background = "#bbb"; 
+			document.getElementById('box6-simple-btn2').style.background = "#ddd"; 
+			document.getElementById('box6-simple-btn3').style.background = "#bbb"; 
+
+			document.getElementById('box6-simple-btn1').style.borderBottom ="solid #000 1px";
+			document.getElementById('box6-simple-btn2').style.borderBottom ="none";
+			document.getElementById('box6-simple-btn3').style.borderBottom ="solid #000 1px";
+			
+			document.getElementById('box6-more1').style.display = "none";
+			document.getElementById('box6-more2').style.display = "block";
+			document.getElementById('box6-more3').style.display = "none";
+			
+			document.getElementById('box6-simple-main1').style.display = "none";
+			document.getElementById('box6-simple-main2').style.display = "block";
+			document.getElementById('box6-simple-main3').style.display = "none";
+		}	
+		else if(x == "box6-simple-btn3"){
+			document.getElementById('box6-simple-btn1').style.background = "#bbb"; // темний
+			document.getElementById('box6-simple-btn2').style.background = "#bbb"; // темний
+			document.getElementById('box6-simple-btn3').style.background = "#ddd"; // светлый
+
+			document.getElementById('box6-simple-btn1').style.borderBottom = "solid #000 1px";
+			document.getElementById('box6-simple-btn2').style.borderBottom = "solid #000 1px";
+			document.getElementById('box6-simple-btn3').style.borderBottom = "none";
+		
+			document.getElementById('box6-more1').style.display = "none";
+			document.getElementById('box6-more2').style.display = "none";
+			document.getElementById('box6-more3').style.display = "block";
+		
+			document.getElementById('box6-simple-main1').style.display = "none";
+			document.getElementById('box6-simple-main2').style.display = "none";
+			document.getElementById('box6-simple-main3').style.display = "block";
+		}
+	})
+})
